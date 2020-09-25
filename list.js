e0c4a22993348d69dc5bd79c17f48be34e0da9d2
Ana <anapaulalins17102000@gmail.com>
2020-09-25 18:24:45 -0300

    added list.js

---
+++ list.js
@@ -0,0 +1,48 @@
+const list = [
+    {
+        title: 'Vereador',
+        number: 5,
+        candidate: [
+            {
+                number: 12345,
+                name: 'Selena Gomes',
+                partido: 'Akitando',
+                fotos: [
+                    { url: 'selena.png', legenda: 'Vereador' }
+                ]
+            },
+            {
+                number: 12365,
+                name: 'Michel Teló',
+                partido: 'Delisinha',
+                fotos: [
+                    { url: 'telo.jpg', legenda: 'Vereador' }
+                ]
+            }
+        ]
+    },
+    {
+        title: 'Prefeito',
+        number: 2,
+        candidate: [
+            {
+                number: 42,
+                name: 'Tio Akita',
+                partido: 'Akitando',
+                fotos: [
+                    { url: 'akita.jpg', legenda: 'Pefeito' },
+                    { url: 'axl.jpg', legenda: 'Vice-Prefeito' }
+                ]
+            },
+            {
+                number: 97,
+                name: 'Tio Bill',
+                partido: 'Win free P/Geral',
+                fotos: [
+                    { url: 'bill.jpg', legenda: 'Prefeito' },
+                    { url: 'martha.jpg', legenda: 'Vice-Prefeita' }
+                ]
+            }
+        ]
+    }
+]
